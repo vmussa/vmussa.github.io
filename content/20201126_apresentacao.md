b738711661c98b4e96aaab074fddc37675ed8ce4
Vitor Mussa <vtrmussa@gmail.com>
2020-11-26 11:16:31 -0300

    Primeiro commit: configs + apresentação

---
+++ content/20201126_apresentacao.md
@@ -0,0 +1,9 @@
+Title: Apresentação
+Date: 2020-11-26 11:14
+Category: Apresentação
+Tags: apresentacao, ciencia-de-dados, sociologia-computacional, sociologia-digital
+Slug: apresentacao
+Authors: Vítor Mussa
+Summary: Apresentação do blog.
+
+Post de apresentação do blog.
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+AUTHOR = 'Vítor Mussa'
+SITENAME = 'Ciência de Dados e Sociologia Computacional e Digital por Vítor Mussa'
+SITEURL = ''
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = 'pt'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (('Pelican', 'https://getpelican.com/'),
+         ('Python.org', 'https://www.python.org/'),
+         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
+         ('You can modify those links in your config file', '#'),)
+
+# Social widget
+SOCIAL = (('You can add links in your config file', '#'),
+          ('Another social link', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
