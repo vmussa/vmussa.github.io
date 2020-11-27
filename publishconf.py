f5e6f38e81d88760d9995f4b0a60f016b6a8b830
Vitor Mussa <vtrmussa@gmail.com>
2020-11-27 14:40:43 -0300

    Melhora layout e publica primeiras postagens

--- publishconf.py
+++ publishconf.py
@@ -22,3 +22,16 @@ DELETE_OUTPUT_DIRECTORY = True
 
 #DISQUS_SITENAME = ""
 GOOGLE_ANALYTICS = "G-BD2LTYCV4K"
+# GOOGLE_ADSENSE = {
+#     'ca_id': 'ca-pub-3187686650481081',
+#     'page_level_ads': True,          # Allow Page Level Ads (mobile)
+#     'ads': {
+#         'aside': '1234561',          # Side bar banner (all pages)
+#         'main_menu': '1234562',      # Banner before main menu (all pages)
+#         'index_top': '1234563',      # Banner after main menu (index only)
+#         'index_bottom': '1234564',   # Banner before footer (index only)
+#         'article_top': '1234565',    # Banner after article title (article only)
+#         'article_bottom': '1234566', # Banner after article content (article only)
+#     }
+# }
+GOOGLE_TAG_MANAGER = 'GTM-NNQ7JK3'
