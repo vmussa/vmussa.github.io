d59c950acf2f7f81ef790f0e1b7e7b627c66dea7
Vitor Mussa <vtrmussa@gmail.com>
2020-11-27 14:40:43 -0300

    Melhora layout e publica primeiras postagens

--- content/20201126_apresentacao.md
+++ content/20201126_apresentacao.md
@@ -1,9 +1,13 @@
 Title: Apresentação
-Date: 2020-11-26 11:14
-Category: Apresentação
-Tags: apresentacao, ciencia-de-dados, sociologia-computacional, sociologia-digital
+Date: 2020-11-26 11:31
+Category: Geral
+Tags: apresentacao, ciencia-de-dados, sociologia-computacional, sociologia-digital, social-data-science, ciencias-de-dados-sociais
 Slug: apresentacao
 Authors: Vítor Mussa
-Summary: Apresentação do blog.
+Summary: Apresentação do blog: dicas e tutoriais sobre ciência de dados, sociologia computacional e digital.
 
-Post de apresentação do blog.
+Esse pequeno blog se dedica a discutir e divulgar conhecimentos sobre ciência de dados, sociologia computacional e digital.
+
+Meu nome é Vítor Mussa. Atualmente faço mestrado no Programa de Pós-Gradução em Sociologia e Antropologia (PPGSA) da Universidade Federal do Rio de Janeiro (UFRJ). Lá eu estudo a relação entre as [plataformas de microtrabalho](https://databigandsmall.com/2019/06/12/microwork-platforms-a-challenge-for-artificial-intelligence-a-challenge-for-employment/) e a agenda da [Indústria 4.0](https://pt.wikipedia.org/wiki/Ind%C3%BAstria_4.0). Para isso, eu utilizo várias técnicas de ciências de dados e computação, como o web scraping (ou raspagem de dados web), processamento de linguagem natural (NLP) e outras técncias de mineração de texto. Me considero parte de um movimento dentro das ciências sociais que busca construir uma sociologia computacional ou digital que se baseia nessas técnicas e reflete epistemologicamente as implicações de um contexto social de produção massiva de dados (Big Data). De forma mais ampla, participo da comunidade de ciência de dados, buscando aprender cada vez mais e ajudar a difundir o que aprendo. A união dessas duas áreas têm sido chamada por alguns de Social Data Science.
+
+Aqui você poderá encontrar conteúdos como pequenos tutoriais, projetos em construção, meu portfólio, dicas, reflexões sociológicas, Python e outras coisas mais. Sinta-se livre para clicar nos botõezinhos das redes sociais ao lado e falar comigo diretamente sobre qualquer coisa. Bem-vindx!
--- pelicanconf.py
+++ pelicanconf.py
@@ -3,7 +3,9 @@
 
 AUTHOR = 'Vítor Mussa'
 SITENAME = 'Ciência de Dados e Sociologia Computacional e Digital por Vítor Mussa'
-SITEURL = ''
+SITEURL = 'https://vmussa.github.io'
+SITETITLE =  'Vítor Mussa'
+SITESUBTITLE = 'Ciência de Dados e Sociologia Computacional/Digital'
 
 PATH = 'content'
 
@@ -19,15 +21,20 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('página inicial', 'https://vmussa.github.io/'),
-         ('portfólio', '#'),
-         ('tutoriais', '#'),)
+LINKS = (
+    ('página inicial', 'https://vmussa.github.io/'),
+    ('portfólio', '#'),
+    ('tutoriais', '#'),
+    ('sobre mim', '#'),
+    ('contato', 'mailto:vtrmussa@gmail.com')
+)
 
 # Social widget
-SOCIAL = (('GitHub', 'https://github.com/vmussa'),
-          ('Twitter', 'https://twitter.com/vitormussa'),
-          ('LinkedIn', 'https://www.linkedin.com/in/vmussa/'),
-          ('Lattes', 'http://lattes.cnpq.br/2934187748254130'),)
+SOCIAL = (
+    ('github', 'https://github.com/vmussa'),
+    ('twitter', 'https://www.twitter.com/vitormussa'),
+    ('linkedin', 'https://www.linkedin.com/in/vmussa/'),
+)
 
 DEFAULT_PAGINATION = 10
 
@@ -39,16 +46,20 @@ THEME = '..\\pelican-themes\\Flex\\'
 
 # Analytics & Monetization
 GOOGLE_ANALYTICS = 'G-BD2LTYCV4K'
-GOOGLE_ADSENSE = {
-    'ca_id': 'ca-pub-3187686650481081',
-    'page_level_ads': True,          # Allow Page Level Ads (mobile)
-    'ads': {
-        'aside': '1234561',          # Side bar banner (all pages)
-        'main_menu': '1234562',      # Banner before main menu (all pages)
-        'index_top': '1234563',      # Banner after main menu (index only)
-        'index_bottom': '1234564',   # Banner before footer (index only)
-        'article_top': '1234565',    # Banner after article title (article only)
-        'article_bottom': '1234566', # Banner after article content (article only)
-    }
-}
+# GOOGLE_ADSENSE = {
+#     'ca_id': 'ca-pub-3187686650481081',
+#     'page_level_ads': True,          # Allow Page Level Ads (mobile)
+#     'ads': {
+#         'aside': '1234561',          # Side bar banner (all pages)
+#         'main_menu': '1234562',      # Banner before main menu (all pages)
+#         'index_top': '1234563',      # Banner after main menu (index only)
+#         'index_bottom': '1234564',   # Banner before footer (index only)
+#         'article_top': '1234565',    # Banner after article title (article only)
+#         'article_bottom': '1234566', # Banner after article content (article only)
+#     }
+# }
 GOOGLE_TAG_MANAGER = 'GTM-NNQ7JK3'
+
+# Tests\development
+LOAD_CONTENT_CACHE = False
+DELETE_OUTPUT_DIRECTORY = False
